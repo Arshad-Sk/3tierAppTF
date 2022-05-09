99957bc37ae82ac70b14610d6e0a9c125b51766d
Arshad Shaikh <sk77arshad@gmail.com>
2022-05-09 22:49:20 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -11,7 +11,6 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-2"
-      access_key = "AKIA2ZMIYGO3OBHYQLJG"
-      secret_key = "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"
+     
  
 }
