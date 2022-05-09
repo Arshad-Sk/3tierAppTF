2c7c66e97a8e97f8beca12f0028a819f3a8c7ff7
Arshad Shaikh <sk77arshad@gmail.com>
2022-05-10 01:45:04 +0530

    Update web_sg.tf

--- web_sg.tf
+++ web_sg.tf
@@ -18,6 +18,14 @@ resource "aws_security_group" "demosg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  
+  ###
+   ingress {
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
 ingress {
     from_port   = 8080
